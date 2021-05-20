2e4ee950f99183d62bb7a3128926f40bb024e13f
Cristina Salazar <cristinasalazarm1@gmail.com>
2021-05-20 16:16:19 +0200

    add list bookmarks controllers

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,11 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: [:show]
-
   def index
     @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
+    @bookmarks = Bookmark.where(:list_id => @list.id)
   end
 
   def new
@@ -23,10 +23,6 @@ class ListsController < ApplicationController
   end
 
   private
-
-  def set_list
-    @list = List.find(params[:id])
-  end
 
   def list_params
     params.require(:list).permit(:name)
